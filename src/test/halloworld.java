67d53bece9ffa43de3f40ecfd3ac6ca98c71b7d5
hataryushichi <hataryushichi@DESKTOP-VUBBO0Q>
2024-01-10 20:30:44 +0900

    test

--- src/test/halloworld.java
+++ src/test/halloworld.java
@@ -5,11 +5,10 @@ public class halloworld {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		
-	for(int i=0;i < 5;i++) {
-		System.out.println("halloworld");
-		System.out.println("halloworld");	
-	}
-	
+		for(int i=0;i < 5;i++) {
+			System.out.println("halloworld");
+		}
+		
 	}
 
 }
