acc1712423d1b7450ab5df1f316bdad47d5b6ed6
hataryushichi <hataryushichi@DESKTOP-VUBBO0Q>
2024-01-10 20:21:56 +0900

    test

--- src/test/halloworld.java
+++ src/test/halloworld.java
@@ -4,8 +4,12 @@ public class halloworld {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-	System.out.println("halloworld");
-	System.out.println("halloworld");
+		
+	for(int i=0;i < 5;i++) {
+		System.out.println("halloworld");
+		System.out.println("halloworld");	
+	}
+	
 	}
 
 }
